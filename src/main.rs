1c19fe2f1ad49d6ccd158954bf53fded7cb3e4da
finlaysr <60317020+finlaysr@users.noreply.github.com>
2026-05-13 22:03:06 +0100

    Changed to using command line arguments to specify file paths

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,155 @@
 use std::{
+    env,
     fs::{self, DirEntry},
-    path::Path,
+    path::{Path, PathBuf},
     process::{Command, Stdio},
     sync::{Arc, Mutex, mpsc},
     thread::{self, JoinHandle},
 };
 
-const NOTEBOOK_FOLDER: &str = "/home/frobb/backups/kindle-scribe notebooks 12-05-2026";
+use indicatif::ProgressBar;
+
+#[derive(Clone)]
+struct Config {
+    notebook_dir: PathBuf,
+    output_dir: PathBuf,
+}
 
 fn main() {
-    println!("Hello, world!");
-    fs::create_dir(Path::new(&format!("{NOTEBOOK_FOLDER}/pdfs"))).ok();
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    let config: Config = match args.len() {
+        ..=1 | 4.. => {
+            panic!(
+                "Arguments Required:
+<.notebooks folder> [output folder]
+ouptut folder is optional, if not set then a new folder will be created next to .notebook",
+            );
+        }
+        2 => {
+            let config = Config {
+                notebook_dir: PathBuf::from(&args[1]),
+                output_dir: PathBuf::from(&args[1]).parent().unwrap().join("output"),
+            };
+            if fs::exists(&config.output_dir).unwrap() {
+                fs::remove_dir_all(&config.output_dir)
+                    .expect("Couldn't remove exisitng output directory");
+            }
+            fs::create_dir(&config.output_dir).expect("Couldn't create output directory");
+            config
+        }
+        3 => Config {
+            notebook_dir: PathBuf::from(&args[1]),
+            output_dir: PathBuf::from(&args[2]),
+        },
+    };
 
-    // iterate through each notebook
-
-    let notebooks: Vec<DirEntry> = fs::read_dir(format!("{NOTEBOOK_FOLDER}/.notebooks"))
+    // Array of all notebooks to be converted
+    let notebooks: Vec<DirEntry> = fs::read_dir(&config.notebook_dir)
         .unwrap()
         .map(|dir| dir.unwrap())
-        // Remove anything that doesn't contain a notebook file
+        // Remove anything that doesn't contain a notebook file or is an annotation file
         .filter(|dir| {
-            !dir.path().to_str().unwrap().contains("!!")
+            !dir.path().to_str().unwrap().contains("!!") // annotation file
                 && fs::read_dir(Path::new(dir.path().to_str().unwrap()))
                     .unwrap()
                     .any(|f| f.unwrap().file_name() == "nbk")
         })
         .collect();
+    let notebook_count = notebooks.len();
+    let prog = Arc::new(Mutex::new(ProgressBar::new(notebook_count as u64)));
 
-    let (sender, receiver) = mpsc::channel();
+    // Receivers and senders for sharing jobs between threads
+    let (sender, receiver) = mpsc::channel::<DirEntry>();
     let receiver = Arc::new(Mutex::new(receiver));
-    let num = std::thread::available_parallelism().unwrap().get();
-    dbg!(num);
+    let threads = std::thread::available_parallelism().unwrap().get();
+    dbg!(threads);
 
-    let handles: Vec<JoinHandle<()>> = (1..=num)
+    // Set up threads
+
+    let handles: Vec<JoinHandle<()>> = (1..=threads)
         .map(|i| {
+            let config = config.clone();
             let receiver = Arc::clone(&receiver);
+            let prog = Arc::clone(&prog);
             thread::spawn(move || {
                 loop {
-                    let job = {
-                        let receiver = receiver.lock().unwrap();
-                        receiver.recv()
-                    };
+                    let job = receiver.lock().unwrap().recv();
                     match job {
                         Ok(dir) => {
-                            println!("\n\nworker {i} working on {:?}", dir);
-                            convert_to_pdf(dir);
+                            prog.lock().unwrap().inc(1);
+                            println!(
+                                "worker {i} working on {}",
+                                dir.file_name().to_str().unwrap()
+                            );
+                            convert_to_pdf(dir, &config);
                         }
                         Err(_) => {
                             println!("worker {i} stopping");
                             break;
                         }
-                    };
+                    }
                 }
             })
         })
         .collect();
 
+    // Send jobs to all threads
     notebooks.into_iter().for_each(|nb| {
         sender.send(nb).unwrap();
     });
-    // Close sender
-    drop(sender);
 
+    // ensure all threads have finished
     handles.into_iter().for_each(|h| {
         h.join().expect("worker thread panicked");
     });
+
+    // Close sender
+    drop(sender);
 }
 
-fn convert_to_pdf(dir: DirEntry) {
+fn convert_to_pdf(dir: DirEntry, config: &Config) {
+    const EPUB_NAME: &str = "notebook.epub";
+    const PDF_NAME: &str = "notebook.pdf";
+
     let name = dir.file_name().into_string().unwrap();
-    println!(" name: {:?}", name);
     // Output folder
-    fs::create_dir(Path::new(&format!("{NOTEBOOK_FOLDER}/pdfs/{name}"))).ok();
+    fs::create_dir(config.output_dir.join(&name)).expect("Couldn't create notebook output folder!");
 
     // Convert notebook to EPUB using calibre and the KFX Input plugin
     Command::new("calibre-debug")
-        .stdout(Stdio::piped())
         .args([
             "-r",
             "KFX Input",
             "--",
-            &format!("{NOTEBOOK_FOLDER}/.notebooks/{name}"),
-            &format!("{NOTEBOOK_FOLDER}/pdfs/{name}/notebook.epub"),
+            config.notebook_dir.join(&name).to_str().unwrap(),
+            config
+                .output_dir
+                .join(&name)
+                .join(EPUB_NAME)
+                .to_str()
+                .unwrap(),
         ])
+        .stdout(Stdio::null())
         .status()
         .expect("Couldn't convert to epub!");
+    println!("Converted to epub");
 
     // Convert EPUB to PDF
     Command::new("ebook-convert")
         .args([
-            &format!("{NOTEBOOK_FOLDER}/pdfs/{name}/notebook.epub"),
-            &format!("{NOTEBOOK_FOLDER}/pdfs/{name}/notebook.pdf"),
+            config
+                .output_dir
+                .join(&name)
+                .join(EPUB_NAME)
+                .to_str()
+                .unwrap(),
+            config
+                .output_dir
+                .join(&name)
+                .join(PDF_NAME)
+                .to_str()
+                .unwrap(),
             "--pdf-page-margin-top",
             "0",
             "--pdf-page-margin-left",
@@ -99,10 +159,11 @@ fn convert_to_pdf(dir: DirEntry) {
             "--pdf-page-margin-bottom",
             "0",
         ])
+        .stdout(Stdio::null())
         .status()
         .expect("Couldn't convert to pdf!");
 
     // Remove EPUB file since no longer needed
-    fs::remove_file(format!("{NOTEBOOK_FOLDER}/pdfs/{name}/notebook.epub"))
+    fs::remove_file(config.output_dir.join(&name).join(EPUB_NAME).as_path())
         .expect("Couldn't remove the epub file!");
 }
